0011b8ea75eebfdba924472a43c66dfd741272f5
dattp6501 <truongphucdat6501@gmail.com>
2024-12-18 17:09:59 +0700

    bo kafka

--- src/main/java/com/dattp/authservice/config/kafka/ProducerKafkaConfig.java
+++ src/main/java/com/dattp/authservice/config/kafka/ProducerKafkaConfig.java
@@ -3,15 +3,15 @@ package com.dattp.authservice.config.kafka;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+//import org.apache.kafka.clients.producer.ProducerConfig;
+//import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+//import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.kafka.core.ProducerFactory;
+//import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import com.dattp.authservice.dto.UserResponseDTO;
 
@@ -31,28 +31,28 @@ public class ProducerKafkaConfig {
     private String SECURITY_PROTOCOL;
 
     //
-    public Map<String, Object> producerConfigJSON(){
-        Map<String,Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, "com.dattp.restaurant.auth.id");
-
-        if(kafkaTest){
-          props.put("sasl.jaas.config", SASL_JAAS_CONFIG);
-          props.put("sasl.mechanism", SASL_MECHANISM);
-          props.put("security.protocol", SECURITY_PROTOCOL);
-        }
-        return props;
-    }
-    // user
-    @Bean
-    public ProducerFactory<String,UserResponseDTO> producerFactoryUser(){
-        return new DefaultKafkaProducerFactory<>(producerConfigJSON());
-    }
-    @Bean
-    public KafkaTemplate<String,UserResponseDTO> kafkaTemplateUser(ProducerFactory<String,UserResponseDTO> producerFactory){
-        return new KafkaTemplate<>(producerFactory);
-    }
+//    public Map<String, Object> producerConfigJSON(){
+//        Map<String,Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+//        props.put(ProducerConfig.CLIENT_ID_CONFIG, "com.dattp.restaurant.auth.id");
+//
+//        if(kafkaTest){
+//          props.put("sasl.jaas.config", SASL_JAAS_CONFIG);
+//          props.put("sasl.mechanism", SASL_MECHANISM);
+//          props.put("security.protocol", SECURITY_PROTOCOL);
+//        }
+//        return props;
+//    }
+//    // user
+//    @Bean
+//    public ProducerFactory<String,UserResponseDTO> producerFactoryUser(){
+//        return new DefaultKafkaProducerFactory<>(producerConfigJSON());
+//    }
+//    @Bean
+//    public KafkaTemplate<String,UserResponseDTO> kafkaTemplateUser(ProducerFactory<String,UserResponseDTO> producerFactory){
+//        return new KafkaTemplate<>(producerFactory);
+//    }
     //
 }
--- src/main/java/com/dattp/authservice/config/kafka/TopicKafkaConfig.java
+++ src/main/java/com/dattp/authservice/config/kafka/TopicKafkaConfig.java
@@ -1,10 +1,10 @@
 package com.dattp.authservice.config.kafka;
 
-import org.apache.kafka.clients.admin.NewTopic;
+//import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
+//import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class TopicKafkaConfig {
@@ -12,13 +12,13 @@ public class TopicKafkaConfig {
     private int partition;
     public static final String NEW_USER_TOPIC = "com.dattp.restaurant.auth.new_user";
 //    public static final String VERIFY_USER_TOPIC = "VERIFY_USER_TOPIC";
-    @Bean
-    public NewTopic newUserTopic(){
-        return TopicBuilder
-          .name(NEW_USER_TOPIC)
-          .partitions(partition)
-          .build();
-    }
+//    @Bean
+//    public NewTopic newUserTopic(){
+//        return TopicBuilder
+//          .name(NEW_USER_TOPIC)
+//          .partitions(partition)
+//          .build();
+//    }
 
 //    @Bean
 //    public NewTopic verifyUserTopic(){
